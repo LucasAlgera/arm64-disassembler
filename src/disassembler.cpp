3d972af72a012ebf1af8947fd1e6b902550cb4f7
Lucas Algera <93121836+LucasAlgera@users.noreply.github.com>
2025-11-14 21:00:53 +0100

    [ADD] Project files
    - Basic CMake setup

---
+++ src/disassembler.cpp
@@ -0,0 +1,6 @@
+#include "disassembler.h"
+#include <iostream>
+void Disassembly()
+{
+	std::cout << "disassemblabla";
+}
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "disassembler.h"
+int main()
+{
+    std::cout << "stuff works \n";
+    Disassembly();
+    return 0;
+}
