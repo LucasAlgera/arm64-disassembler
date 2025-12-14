a44d356d6a2223b54995ec66e76880e647f917d7
Lucas Algera <93121836+LucasAlgera@users.noreply.github.com>
2025-12-14 14:54:29 +0100

    [ADD] Updated CMAKE file for newly added files, added VS filters in cmake.

---
+++ include/decoders/decode_data_proc_reg.h
@@ -0,0 +1,10 @@
+// ---------------------------------
+// All decoders for the subcategory:
+// DATA PROCESSING REGISTER
+// ---------------------------------
+
+
+#include <string>
+
+
+std::string DecodeDATA_PROC_1(uint32_t instruction);
---
+++ src/decoders/decode_data_proc_reg.cpp
@@ -0,0 +1,6 @@
+#include "decoders/decode_data_proc_reg.h"
+
+std::string DecodeDATA_PROC_1(uint32_t instruction)
+{
+	return std::string();
+}
